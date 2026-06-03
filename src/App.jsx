15e5d505f10f9f116571787b844eaa0b77df6fde
kapilchandrawal <77915124+kapilchandrawal@users.noreply.github.com>
2026-06-03 21:23:03 +0530

    Created Header UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import Animate from "./components/Animate";
+import Notification from "./components/Notification";
+import Header from "./components/Header";
+import StatsGrid from "./components/StatsGrid";
+import Input from "./components/Input";
+import TodoList from "./components/TodoList";
+import ClearButton from "./components/ClearButton";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <div className="min-h-screen bg-linear-to-br from-indigo-950 via-purple-950 to-pink-950 p-3 sm:p-6 relative overflow-hidden">
+        <Animate />
+        <Notification />
 
-export default App
+        <div className="max-w-3xl mx-auto relative z-10">
+          <Header />
+          <StatsGrid />
+          <Input />
+          <TodoList />
+          <ClearButton />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Sparkles, TrendingUp  } from "lucide-react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <div className="backdrop-blur-2xl bg-white/5 rounded-3xl border-white/10 p-6 mb-4 shadow-2xl">
+        <div className="flex items-center justify-between mb-4">
+          
+          {/* Left side part */}
+          <div className="flex items-center gap-3">
+            <div className="relative">
+              <div className="w-12 h-12 bg-linear-to-br from-violet-500 via-purple-500 to-fuchsia-500 rounded-2xl flex items-center justify-center shadow-lg">
+                <Sparkles className="text-white" size={24} />
+              </div>
+              <div className="absolute -top-1 -right-1 w-4 h-4 bg-emerald-400 rounded-full animate-ping"></div>
+            </div>
+            <div>
+              <h1 className="text-3xl font-black text-white tracking-tight">
+                TaskFlow
+              </h1>
+              <p className="text-purple-300 text-xs font-medium">
+                Productivity Reimagined
+              </p>
+            </div>
+          </div>
+          
+          {/* Right side part */}
+          <div className="flex items-center gap-2 px-2 py-2 bg-linear-to-br from-violet-500/20 to-fuchsia-500/20 rounded-full border border-violet-400/30">
+            <TrendingUp size={16} className="text-emerald-400" />
+            <span className="text-white font-bold text-sm">{1} Active</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
