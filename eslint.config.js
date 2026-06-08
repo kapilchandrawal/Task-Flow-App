86d5a32ac3f6aff0f14bc565805ecd196266ed2d
kapilchandrawal <77915124+kapilchandrawal@users.noreply.github.com>
2026-06-08 22:38:51 +0530

    Created TodoList and TodoItem components

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -17,5 +18,9 @@ export default defineConfig([
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    rules: {
+      "no-unused-vars": "warn", //on, off
+    },
   },
-])
+  eslintConfigPrettier,
+]);
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import TodoItem from "./TodoItem";
 
 const TodoList = () => {
-  return (
-    <div>TodoList</div>
-  )
-}
+  let todos = [];
+  let editingId;
+  let editText;
+  let onToggle;
+  let onStartEdit;
+  let onSaveEdit;
+  let onCancleEdit;
+  let onDelete;
+  let onEditTextChange;
+  let onEditKeyPress;
 
-export default TodoList
+  return (
+    <>
+      <div className="space-y-2">
+        {todos.map((todo, index) => {
+          <TodoItem
+            key={index}
+            todo={todo}
+            index={index}
+            editingId={editingId}
+            editText={editText}
+            onToggle={onToggle}
+            onStartEdit={onStartEdit}
+            onSaveEdit={onSaveEdit}
+            onCancleEdit={onCancleEdit}
+            onDelete={onDelete}
+            onEditTextChange={onEditTextChange}
+            onEditKeyPress={onEditKeyPress}
+          />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default TodoList;
